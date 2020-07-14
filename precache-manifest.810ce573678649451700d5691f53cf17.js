817a28bc19c4b49d973e22f5b353a8cb3e2fe9f6
IvanRi <riboldiip@globalhitss.com>
2020-07-13 23:06:52 -0300

    Updates

---
+++ precache-manifest.810ce573678649451700d5691f53cf17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae08961587ece87beb32624b497deb59",
+    "url": "/turnos/index.html"
+  },
+  {
+    "revision": "fe05bd432ef6e2387fe2",
+    "url": "/turnos/static/js/2.d94bd57f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/turnos/static/js/2.d94bd57f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2811c3b9387703fe69a",
+    "url": "/turnos/static/js/main.2e234b94.chunk.js"
+  },
+  {
+    "revision": "1532e753cc65de21da76",
+    "url": "/turnos/static/js/runtime-main.26641059.js"
+  }
+]);
